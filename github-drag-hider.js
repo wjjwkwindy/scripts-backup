97cbeb4b2c53776aeb37b76fe5b5ef2ce8308710
wjjwkwindy <wjjwkwindy@gmail.com>
2018-05-23 23:27:27 +0800

    feat: 修改按钮样式
    1.:lipstick:修改按钮样式。

--- github-drag-hider.js
+++ github-drag-hider.js
@@ -2,7 +2,7 @@
 // @name         Github drag disable
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  Disable github drap to reorder
+// @description  Disable github drap to reorder icon
 // @author       wjjwkwindy
 // @match        *://github.com/*
 // @grant        none
@@ -11,19 +11,24 @@
 (function () {
     'use strict';
 
-    var dragIcon = document.getElementsByClassName('pinned-repository-handle');
-    var pinnedRepositories = document.getElementsByClassName('js-pinned-repos-reorder-container')[0];
-    var pinnedRepositoriesH2 = pinnedRepositories.childNodes[3];
-    if (pinnedRepositories.innerText.indexOf('Pinned repositories') != -1) {
-        var hideDrag = document.createElement('button');
-        hideDrag.innerText = 'click to hide drag';
-        hideDrag.setAttribute('class', 'hide-drag');
-        pinnedRepositoriesH2.appendChild(hideDrag);
+    var css=document.createElement('style');
+    css.type='text/css';
+    css.innerText='.hide-drag{width:40px;height:20px;border:1px solid #13ce66;background-color:#13ce66;border-radius:10px;box-sizing:border-box;margin-right:5px;position:relative;top:4px;float:right;cursor:pointer}.hide-drag:after{content:"";width:16px;height:16px;background-color:#fff;border-radius:100%;position:absolute;top:1px;left:1px;transition:.3s}.active:after{left:20px}';
+    document.getElementsByTagName('head')[0].appendChild('css');
+
+    var dragIconElem = document.getElementsByClassName('pinned-repository-handle');
+    var prElem = document.getElementsByClassName('js-pinned-repos-reorder-container')[0];
+    var prH2Elem = prElem.childNodes[3];
+    if (prElem.innerText.indexOf('Pinned repositories') != -1) {
+        var hideDrag = document.createElement('span');
+        hideDrag.setAttribute('class', 'hide-drag active');
+        prH2Elem.appendChild(hideDrag);
     }
+
     hideDrag.onclick = function () {
         console.log('you clicked hide-drag');
-        for (let i = 0; i < dragIcon.length; i++) {
-            dragIcon.item(i).style.display = (dragIcon.item(i).style.display == 'none') ? 'block' : 'none';
+        for (let i = 0; i < dragIconElem.length; i++) {
+            dragIconElem.item(i).style.display = (dragIconElem.item(i).style.display == 'none') ? 'block' : 'none';
         }
     };
 })();
