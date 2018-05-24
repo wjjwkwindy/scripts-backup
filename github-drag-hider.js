c3ab41c49cfdb4e99e2e5b8136dab3ca9ab8cc7d
wjjwkwindy <wjjwkwindy@gmail.com>
2018-05-24 23:27:23 +0800

    feat: 添加存储用户设置的功能
    1.现在可以存储和读取用户的设置(使用localStorage)。
    2.使用存储的值来更新用户界面。

--- github-drag-hider.js
+++ github-drag-hider.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Github drag disable
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      1.0
 // @description  Disable github drap to reorder icon
 // @author       wjjwkwindy
 // @match        *://github.com/*
@@ -11,24 +11,56 @@
 (function () {
     'use strict';
 
-    var css=document.createElement('style');
-    css.type='text/css';
-    css.innerText='.hide-drag{width:40px;height:20px;border:1px solid #13ce66;background-color:#13ce66;border-radius:10px;box-sizing:border-box;margin-right:5px;position:relative;top:4px;float:right;cursor:pointer}.hide-drag:after{content:"";width:16px;height:16px;background-color:#fff;border-radius:100%;position:absolute;top:1px;left:1px;transition:.3s}.active:after{left:20px}';
-    document.getElementsByTagName('head')[0].appendChild('css');
+    var iconCss = document.createElement('style');
+    iconCss.type = 'text/css';
+    iconCss.innerText = '.hide-drag{width:40px;height:20px;border:1px solid rgb(255, 73, 73);background-color:rgb(255, 73, 73);border-radius:10px;box-sizing:border-box;margin-right:5px;position:relative;top:4px;float:right;cursor:pointer}.hide-drag:after{content:"";width:16px;height:16px;background-color:#fff;border-radius:100%;position:absolute;top:1px;left:1px;transition:.3s}.active{border:1px solid #13ce66;background-color:#13ce66;}.active:after{left:20px}';
+    document.getElementsByTagName('head')[0].appendChild(iconCss);
 
     var dragIconElem = document.getElementsByClassName('pinned-repository-handle');
     var prElem = document.getElementsByClassName('js-pinned-repos-reorder-container')[0];
     var prH2Elem = prElem.childNodes[3];
     if (prElem.innerText.indexOf('Pinned repositories') != -1) {
         var hideDrag = document.createElement('span');
-        hideDrag.setAttribute('class', 'hide-drag active');
+        hideDrag.setAttribute('class', 'hide-drag');
+        hideDrag.setAttribute('data-allowDrag', 'true');
         prH2Elem.appendChild(hideDrag);
     }
 
+    function initSetting() {
+        var dragSetting = localStorage.getItem('allowDrag');
+        if (dragSetting) {
+            setDragIconElem(dragSetting);
+        } else {
+            console.log('can\'t find localstorage');
+            setDragIconElem('true');
+        }
+    }
+
+    function setDragIconElem(operate) {
+        if (operate == 'true') {
+            hideDrag.classList.add('active');
+            localStorage.setItem('allowDrag', 'true');
+        } else {
+            hideDrag.classList.remove('active');
+            localStorage.setItem('allowDrag', 'false');
+        }
+        hideDrag.setAttribute('data-allowDrag', operate);
+        for (let i = 0; i < dragIconElem.length; i++) {
+            dragIconElem.item(i).style.display = (operate == 'true') ? 'block' : 'none';
+        }
+    }
+
+    window.onload = initSetting();
+
     hideDrag.onclick = function () {
         console.log('you clicked hide-drag');
-        for (let i = 0; i < dragIconElem.length; i++) {
-            dragIconElem.item(i).style.display = (dragIconElem.item(i).style.display == 'none') ? 'block' : 'none';
+        if (hideDrag.getAttribute('data-allowDrag') == 'true') {
+            // 隐藏拖动按钮
+            setDragIconElem('false');
+        } else {
+            // 显示拖动按钮
+            setDragIconElem('true');
         }
     };
+
 })();
